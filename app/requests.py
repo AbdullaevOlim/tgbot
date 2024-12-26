7827edcbdb0788e892c1fcc40b4ea37a55ab3bb9
AbdullaevOlim <113762424+AbdullaevOlim@users.noreply.github.com>
2024-12-26 13:12:31 +0300

    Update requests.py

--- app/requests.py
+++ app/requests.py
@@ -1,9 +1,6 @@
-from sqlalchemy.future import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from models import async_session, Book, Branch
-
-def example_function():
-    return
+#from sqlalchemy.future import select
+#from sqlalchemy.ext.asyncio import AsyncSession
+#from models import async_session, Book, Branch
 
 async def get_book_by_title_and_branch(title: str, branch_name: str):
     async with async_session() as session:
