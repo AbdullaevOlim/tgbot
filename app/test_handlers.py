a783b20980483d448abf6ac0602130c3e2c9bf98
AbdullaevOlim <113762424+AbdullaevOlim@users.noreply.github.com>
2024-12-27 13:43:01 +0300

    Create test_handlers.py

---
+++ app/test_handlers.py
@@ -0,0 +1,33 @@
+import pytest
+from aiogram.types import Message
+from handlers import cmd_start, cmd_books
+from unittest.mock import AsyncMock
+
+
+@pytest.mark.asyncio
+async def test_cmd_start():
+    """Тест для команды /start."""
+    message = AsyncMock()
+    await cmd_start(message)
+    # Проверяем, что бот отправил правильный ответ
+    message.answer.assert_called_once_with(
+        "Привет! Я бот для обслуживания библиотеки.\n"
+        "Вот что я могу:\n"
+        "/count_books - Посчитать количество экземпляров книги в филиале.\n"
+        "/count_faculties - Посчитать факультеты, на которых используется книга в филиале.\n"
+        "/add_book - Добавить новую книгу в библиотеку.\n"
+        "/add_branch - Добавить новый филиал в библиотеку.\n"
+        "/edit_book - Изменить информацию о книге.\n"
+        "/edit_branch - Изменить информацию о филиале.\n"
+        "/books - Вывести список кни\n"
+    )
+@pytest.mark.asyncio
+async def test_stub_one():
+    assert True  # Этот тест всегда успешен
+@pytest.mark.asyncio
+async def test_stub_two():
+    assert True  # Этот тест всегда успешен
+
+@pytest.mark.asyncio
+async def test_stub_three():
+    assert True  # Этот тест всегда успешен
